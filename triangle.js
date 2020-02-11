248738dba798e111dca541241f7b671dc87c18c8
hfab-95 <59997404+hfab-95@users.noreply.github.com>
2020-02-11 10:53:37 +0100

    Update triangle.js

--- triangle.js
+++ triangle.js
@@ -1,3 +1,8 @@
 /**
 * Write a loop that makes seven calls to console.log to output the following triangle
 */
+var content = '';
+for (var i = 0; i < 7; i++) {
+    content = content + '#';
+    console.log(content);
+}
